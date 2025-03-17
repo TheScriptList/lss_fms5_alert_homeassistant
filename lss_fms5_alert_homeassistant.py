bf0691bb9e28997474a5efb44591e6ba932fd8b1
Mister X <MisterX2000@users.noreply.github.com>
2025-03-17 17:12:46 +0100

    add homeassistant_api and much more

--- lss_fms5_alert_homeassistant.py
+++ lss_fms5_alert_homeassistant.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
 """
 Dieses Skript überwacht Fahrzeuge mit Status FMS 5 im Leitstellenspiel und steuert eine Lampe über Home Assistant entsprechend dem Status.
 """
 
 import requests
-import time
 import os
-import argparse
+import logging
+from time import sleep
 from http.cookiejar import MozillaCookieJar
 from homeassistant_api import Client
 from dotenv import load_dotenv, set_key
@@ -14,28 +15,21 @@ from inquirer.shortcuts import text as text_input, confirm as confirm_input, lis
 __version__ = "1.0.0"
 __author__ = "L0rdEnki, MisterX2000"
 
-
 LSS_API_URL = "https://www.leitstellenspiel.de/api/vehicle_states"
-LSS_API_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-    "Accept": "application/json, text/javascript, */*; q=0.01",
-    "Referer": "https://www.leitstellenspiel.de/"}
+HASS_TEMP_SCENE = "lss_fms5_alert_temp_scene"
 
 # Konfigurationsdatei
 CONFIG_FILE = ".env"
 COOKIE_FILE = "cookies.txt"
-load_dotenv(CONFIG_FILE)
+
+# Objekte Initialisieren
+log = logging.getLogger(__name__)
 
 # Function to get all light entities
-def get_all_lights(client):
-    entitie_groups = client.get_entities()
-    light_group = entitie_groups.get('light', {})
-    return light_group.entities
-
 def get_setting(name, message, choices = None, default = None):
     env_val = os.getenv(name)
     if env_val:
-        print(f"{name} = {(env_val[:77] + '...') if len(env_val) > 80 else env_val}")
+        log.info(f"{name} = {(env_val[:77] + '...') if len(env_val) > 80 else env_val}")
         return env_val
 
     if choices:
@@ -47,114 +41,79 @@ def get_setting(name, message, choices = None, default = None):
 
     return ans
 
-
-def load_cookies():
-    if not os.path.exists(COOKIE_FILE):
-        print("Fehlende cookies.txt Datei!")
+def load_cookies(file_path):
+    cookie_jar = MozillaCookieJar()
+    try:
+        cookie_jar.load(file_path, ignore_discard=True, ignore_expires=True)
+    except FileNotFoundError:
+        log.error(COOKIE_FILE + " nicht gefunden")
         exit(1)
-    cookies = MozillaCookieJar(COOKIE_FILE)
-    cookies.load(ignore_discard=True, ignore_expires=True)
-    return cookies
+    return cookie_jar
 
+def get_all_lights(client):
+    entitie_groups = client.get_entities()
+    light_group = entitie_groups.get('light', {})
+    return light_group.entities
 
 def fetch_fms5_count():
-    cookies = load_cookies()
     try:
-        response = requests.get(LSS_API_URL, headers=LSS_API_HEADERS, cookies=cookies)
+        response = requests.get(LSS_API_URL, cookies=COOKIES)
         response.raise_for_status()
         data = response.json()
         return data.get("5", 0)
     except Exception as e:
-        print(f"Fehler bei der API-Abfrage: {e}")
+        log.error(f"Fehler bei der LSS-API-Abfrage: {e}")
         return 0
 
 
-def control_light(api_url, ha_token, entity_id, state, brightness=35, rgb_color=[255, 0, 0], color_temp_kelvin=None, flash=False):
-    headers = {
-        "Authorization": f"Bearer {ha_token}",
-        "Content-Type": "application/json"
-    }
-    data = {"entity_id": entity_id}
-    if state == "turn_on":
-        data["brightness"] = brightness
-        if color_temp_kelvin:
-            data["color_temp_kelvin"] = color_temp_kelvin
-        else:
-            data["rgb_color"] = rgb_color
-        if flash:
-            data["flash"] = "short"
+if __name__ == "__main__":
+    # Einstellungen aus der Environment laden
+    load_dotenv(CONFIG_FILE)
 
-    response = requests.post(f"{api_url}/services/light/{state}", headers=headers, json=data)
+    # Logging einrichten
+    logging.basicConfig(
+        level=os.getenv("LOGGING_LEVEL", "INFO"),
+        format="%(asctime)s %(name)-8s %(levelname)-8s %(message)s")
+    log.info("v" + str(__version__))
 
+    HASS_URL = get_setting("HASS_API_URL", message="Bitte die Home Assistant API URL eingeben")
+    HASS_TOKEN = get_setting("HASS_API_TOKEN", message="Bitte den Home Assistant API-Token eingeben")
+    CHECK_INTERVAL = int(get_setting("CHECK_INTERVAL", message="Bitte das Prüfintervall in Sekunden eingeben", default=10))
+    LIGHT_BRIGHTNESS = int(get_setting("LIGHT_BRIGHTNESS", message="Helligkeit der Lampe [0-255]", default=50))
+    COOKIES = load_cookies(COOKIE_FILE)
 
-def get_light_state(hass_client, entity_id):
-    headers = {"Authorization": f"Bearer {ha_token}"}
-    response = requests.get(f"{api_url}/states/{entity_id}", headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    return None
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action="store_true", help="Debug Modus aktivieren")
-    args = parser.parse_args()
-    debug_mode = args.verbose
-    
-    
-    hass_url = get_setting("HA_API_URL", message="Bitte die Home Assistant API URL eingeben")
-    hass_token = get_setting("HA_TOKEN", message="Bitte den Home Assistant API-Token eingeben")
-    check_interval= get_setting("CHECK_INTERVAL", message="Bitte das Prüfintervall in Sekunden eingeben", default=10)
-    
-    client = Client(hass_url, hass_token)
+    # HASS Client einrichten
+    client = Client(HASS_URL, HASS_TOKEN)
     entity_id = get_setting("ENTITY_ID", message="Bitte die entity_id der Lampe in Home Assistant eingeben",
                              choices=[(light.state.attributes.get("friendly_name", light.entity_id), light.entity_id) for light in get_all_lights(client).values()])
-    
-    exit()
+    light = client.get_domain("light")
+    scene = client.get_domain("scene")
     
     last_fms5_count = 0
-    last_light_state = client.get_entity(entity_id = entity_id)
-  
-    fms5_count = fetch_fms5_count()
-    print(f"Die Leitstelle meldet, dass wir aktuell {fms5_count} Fahrzeuge im FMS 5 haben.")
+    scene.create(scene_id=HASS_TEMP_SCENE, snapshot_entities=entity_id) # Temp Scene zum wiederherstellen erstellen
     
     try:
         while True:
             fms5_count = fetch_fms5_count()
+            log.info(f"Aktuelle FMS5 Anzahl: {fms5_count}")
             if fms5_count > 0:
-                control_light(api_url, ha_token, entity_id, "turn_on", brightness=35, rgb_color=[255, 0, 0])
+                light.turn_on(entity_id, brightness=LIGHT_BRIGHTNESS, rgb_color=[255, 0, 0])
                 if fms5_count > last_fms5_count and last_fms5_count > 0:
-                    time.sleep(0.5)
-                    control_light(api_url, ha_token, entity_id, "turn_on", flash=True)
+                    sleep(0.5)
+                    light.turn_on(entity_id, flash="short")
             else:
                 if last_fms5_count == 0:
-                    last_light_state = get_light_state(api_url, ha_token, entity_id)
-                elif last_light_state and last_light_state.get("state") == "on":
-                    control_light(api_url, ha_token, entity_id, "turn_on", brightness=last_light_state["attributes"].get("brightness", 255), 
-                                  rgb_color=last_light_state["attributes"].get("rgb_color", [255, 255, 255]),
-                                  color_temp_kelvin=last_light_state["attributes"].get("color_temp_kelvin"))
+                    # Temp Scene zum wiederherstellen aktualisieren, falls keine FMS5
+                    scene.create(scene_id=HASS_TEMP_SCENE, snapshot_entities=entity_id)
                 else:
-                    control_light(api_url, ha_token, entity_id, "turn_off")
+                    scene.turn_on(entity_id=f'scene.{HASS_TEMP_SCENE}')
             
             last_fms5_count = fms5_count
             
-            if debug_mode:
-                for i in range(check_interval, 0, -1):
-                    print(f"Nächste Prüfung in {i} Sekunden.......", end="\r")
-                    time.sleep(1)
-                fms5_count = fetch_fms5_count()
-                print(f"Aktuell haben wir {fms5_count} Fahrzeuge im FMS 5.")
-            else:
-                time.sleep(check_interval)
+            log.debug(f"Nächste Prüfung in {CHECK_INTERVAL} Sekunden.......")
+            sleep(CHECK_INTERVAL)
     except KeyboardInterrupt:
-        if last_light_state and last_light_state.get("state") == "on":
-            control_light(api_url, ha_token, entity_id, "turn_on", brightness=last_light_state["attributes"].get("brightness", 255), 
-                          rgb_color=last_light_state["attributes"].get("rgb_color", [255, 255, 255]),
-                          color_temp_kelvin=last_light_state["attributes"].get("color_temp_kelvin"))
-        else:
-            control_light(api_url, ha_token, entity_id, "turn_off")
-        print("\nSkript beendet.")
-
-
-if __name__ == "__main__":
-    main()
+        # Restore the light to its original state and cleanup
+        log.info("KeyboardInterrupt festgestellt, stelle Ursprungszustand wieder her...")
+        scene.turn_on(entity_id=f'scene.{HASS_TEMP_SCENE}')
+        scene.delete(entity_id=f'scene.{HASS_TEMP_SCENE}')
